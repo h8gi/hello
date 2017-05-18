be5219f356305fa31550cc5400780b23e93a97f5
hiroki yagi <hiro.yagi0245@gmail.com>
2017-05-18 12:31:23 +0900

    use str pointer to check null

--- models/user.go
+++ models/user.go
@@ -4,9 +4,9 @@ import "github.com/jinzhu/gorm"
 
 type User struct {
 	gorm.Model
-	Name     string `json:"name" form:"name" validate:"required"`
-	Password string `json:"password" form:"password" validate:"required"`
-	Email    string `json:"email" form:"email" validate:"required,email"`
+	Name     *string `json:"name" form:"name" gorm:"not null;unique"`
+	Password *string `json:"password" form:"password" gorm:"not null"`
+	Email    *string `json:"email" form:"email" gorm:"not null"`
 }
 
 // https://echo.labstack.com/guide/request
